1de65fccac71629ae6e74056672745010726a819
KAWAUchi1013 <93497980+KAWAUchi1013@users.noreply.github.com>
2021-11-19 12:37:06 +0900

    変更修正５

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,20 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new(comment_params)
+   @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to prototype_path(@comment.prototype) # 今回の実装には関係ありませんが、このようにPrefixでパスを指定することが望ましいです。
+      redirect_to prototype_path(@comment.prototype)
+      # 今回の実装には関係ありませんが、このようにPrefixでパスを指定することが望ましいです。
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
-      render "prototypes/show" # views/tweets/show.html.erbのファイルを参照しています。
-    end
+      render "prototypes/show" 
+      # views/tweets/show.html.erbのファイルを参照しています。
+   end
   end
   # createアクション内のtweetと書かれている部分をプロトタイプに変える
 
   def comment_params
-    params.require(:comment).permit(:text).merge(user_id: current_user.id,prototype_id: params[:prototype_id])
+    params.require(:comment).permit(:text).merge(user_id:current_user.id,prototype_id: params[:prototype_id])
     # permitの内容をtextにする
   end
 end
