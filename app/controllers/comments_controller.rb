2d4ab7b26c61bf000ac2a2268e4dce3ec9e8c6ad
KAWAUchi1013 <93497980+KAWAUchi1013@users.noreply.github.com>
2021-11-19 13:12:33 +0900

     変更修正8

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,14 @@
 class CommentsController < ApplicationController
-  def create
-   @comment = Comment.new(comment_params)
+  def create@comment = Comment.new(comment_params)
     if @comment.save
-     redirect_to prototype_path(@comment.prototype)
-    else
-      @prototype = @comment.prototype
-      @comments = @prototype.comments
-      render"prototypes/show"
+      redirect_to prototype_path(@comment.prototype)
+    else@prototype = @comment.prototype
+        @comments = @prototype.comments
+        render "prototypes/show"
+      end
+    end
+    def comment_params
+      params.require(:comment).permit(:text).merge(user_id:current_user.id,prototype_id: params[:prototype_id])
     end
   end
-  def comment_params
-      params.require(:comment).permit(:text).merge(user_id:current_user.id,prototype_id: params[:prototype_id])
-  end
-end
+  
